ac54c0b815d8353c8154f8154bca0f8b775b90a4
Deepak Chopra <88072239+DeepakChopra0@users.noreply.github.com>
2025-02-11 11:02:18 +0530

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -14,9 +14,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  TextEditingController controller = TextEditingController(
-      text:
-          "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=4480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  TextEditingController controller = TextEditingController();
   String imageUrl = "";
 
   void setImageDataToHTMLCode(String url) {
